6718689074a8e0437d98d36af0b7b1bd3ab7100a
Blecault <mateusjp789@gmail.com>
2025-06-07 12:56:52 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -42,6 +42,35 @@ def limpar_tela():
     global todos_valores
     todos_valores = ""
     valor_texto.set("")
+  
+  botoes = [
+    ("C", limpar_tela, 0, 0, 11),
+    ("%", lambda: entrar_valor("%"), 118, 0, 5),
+    ("/", lambda: entrar_valor("/"), 177, 0, 5),
+    ("7", lambda: entrar_valor("7"), 0, 52, 5),
+    ("8", lambda: entrar_valor("8"), 59, 52, 5),
+    ("9", lambda: entrar_valor("9"), 118, 52, 5),
+    ("*", lambda: entrar_valor("*"), 177, 52, 5),
+    ("4", lambda: entrar_valor("4"), 0, 104, 5),
+    ("5", lambda: entrar_valor("5"), 59, 104, 5),
+    ("6", lambda: entrar_valor("6"), 118, 104, 5),
+    ("-", lambda: entrar_valor("-"), 177, 104, 5),
+    ("1", lambda: entrar_valor("1"), 0, 156, 5),
+    ("2", lambda: entrar_valor("2"), 59, 156, 5),
+    ("3", lambda: entrar_valor("3"), 118, 156, 5),
+    ("+", lambda: entrar_valor("+"), 177, 156, 5),
+    ("0", lambda: entrar_valor("0"), 0, 208, 11),
+    (".", lambda: entrar_valor("."), 118, 208, 5),
+    ("=", calcular, 177, 208, 5)
+]
+
+for texto, comando, x, y, largura in botoes:
+    bg_cor = co4 if texto in ["*", "/", "-", "+", "="] else co3
+    fg_cor = co1 if texto in ["*", "/", "-", "+", "="] else fundo
+
+
+
+janela.mainloop()
 
 
 
